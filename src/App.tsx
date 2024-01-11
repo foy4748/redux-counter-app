dd5ae0c8c9240a078db29b002a94df0cd9bd177d
foy4748 <faisaljfcl@gmail.com>
2024-01-11 22:30:16 +0600

    Intial Setup | Redux-Toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { useAppDispatch, useAppSelector } from "./hooks/useRedux";
+import { increment, decrement } from "./redux/counterSlice";
 function App() {
-  return <>Counter: 0</>;
+  const { counter } = useAppSelector((state) => state);
+  const dispatch = useAppDispatch();
+  return (
+    <>
+      <section className="border h-screen flex justify-center items-center">
+        <div>
+          <h1>Count: {counter.value}</h1>
+          <div className="flex justify-between">
+            <button onClick={() => dispatch(increment())}>+</button>
+            <button onClick={() => dispatch(decrement())}>-</button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default App;
