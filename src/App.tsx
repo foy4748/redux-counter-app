22ffb02c058a21ab4e0b44fe45d472fcc0fe7e41
foy4748 <faisaljfcl@gmail.com>
2024-01-11 23:57:03 +0600

    Created the Counter with Tally | v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,44 @@
 import { useAppDispatch, useAppSelector } from "./hooks/useRedux";
+import {
+  LuTally1,
+  LuTally2,
+  LuTally3,
+  LuTally4,
+  LuTally5,
+} from "react-icons/lu";
 import { increment, decrement } from "./redux/counterSlice";
 function App() {
   const { counter } = useAppSelector((state) => state);
   const dispatch = useAppDispatch();
+
+  type TallyProp = {
+    count: number;
+  };
+  const Tally = ({ count }: TallyProp) => {
+    const numberOfTally = Math.floor(count / 5);
+    const tallyMark = count % 5;
+    const tallySet = [
+      <span>
+        <LuTally1 />
+      </span>,
+      <span>
+        <LuTally2 />
+      </span>,
+      <span>
+        <LuTally3 />
+      </span>,
+      <span>
+        <LuTally4 />
+      </span>,
+      <span>
+        <LuTally5 />
+      </span>,
+    ];
+    const tallies = Array(numberOfTally).fill(tallySet[4]);
+    tallies.push(tallySet[tallyMark - 1]);
+    return tallies;
+  };
+
   return (
     <>
       <section className="border h-screen flex justify-center items-center">
@@ -11,6 +47,9 @@ function App() {
           <div className="flex justify-between">
             <button onClick={() => dispatch(increment())}>+</button>
             <button onClick={() => dispatch(decrement())}>-</button>
+          </div>
+          <div className="flex">
+            <Tally count={counter.value} />
           </div>
         </div>
       </section>
